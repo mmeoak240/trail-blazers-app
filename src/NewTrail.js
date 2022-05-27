0ded0179ab4fc390639b12639656a889ef3f4b32
Michael Meoak <mmeoak240@gmail.com>
2022-05-26 17:24:06 -0700

    created useState for NewTrail

--- src/NewTrail.js
+++ src/NewTrail.js
@@ -1,10 +1,36 @@
+import React, { useState } from "react";
+
 const NewTrail = () => {
+	const [formData, setFormData] = useState({
+		image: "",
+		trailName: "",
+		location: "",
+		difficulty: "",
+		description: "",
+	});
+
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type="text" onChange={handleFirstNameChange} value={name} />
-			<input type="text" onChange={handleLastNameChange} value={location} />
-			<input type="text" onChange={handleLastNameChange} value={difficulty} />
-			<input type="text" onChange={handleLastNameChange} value={description} />
+			<input
+				type="text"
+				onChange={handleFirstNameChange}
+				value={formData.trailName}
+			/>
+			<input
+				type="text"
+				onChange={handleLastNameChange}
+				value={formData.location}
+			/>
+			<input
+				type="text"
+				onChange={handleLastNameChange}
+				value={formData.difficulty}
+			/>
+			<input
+				type="text"
+				onChange={handleLastNameChange}
+				value={formData.description}
+			/>
 			<button type="submit">Submit</button>
 		</form>
 	);
--- src/TrailsContainer.js
+++ src/TrailsContainer.js
@@ -3,26 +3,7 @@ import { Route, useRouteMatch } from "react-router-dom";
 import TrailCard from "./TrailCard";
 import TrailDescription from "./TrailDescription";
 
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		paper: {
-			padding: theme.spacing(2),
-			textAlign: "center",
-			color: theme.palette.text.secondary,
-		},
-		root: {
-			flexGrow: 1,
-		},
-	})
-);
-
 const TrailsContainer = ({ trails }) => {
-	const classes = useStyles();
-
 	const match = useRouteMatch();
 	const displayTrails = trails.map((trail) => (
 		<TrailCard trail={trail} trails={trails} />
@@ -33,14 +14,12 @@ const TrailsContainer = ({ trails }) => {
 				Blaze These Trails
 			</h1>
 
-			<Grid container spacing={3}>
-				<Grid item xs={6} sm={3}>
-					{displayTrails}
-				</Grid>
+			<div>
+				{displayTrails}
 				<Route path={`${match.url}/:trailId`}>
-					<TrailDescription trails={trails} className={classes.paper} />
+					<TrailDescription trails={trails} />
 				</Route>
-			</Grid>
+			</div>
 		</div>
 	);
 };
