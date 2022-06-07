b38752b34f5c8ffb3f79c17a99cdedbaaa1fc38c
mmeoak240 <97415262+mmeoak240@users.noreply.github.com>
2022-06-07 12:40:58 -0700

    Update TrailsContainer.js

--- src/TrailsContainer.js
+++ src/TrailsContainer.js
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { Route, useRouteMatch } from "react-router-dom";
+import React from "react";
 import TrailCard from "./TrailCard";
-import TrailDescription from "./TrailDescription";
-import SortSearch from "./SortSearch";
+import Search from "./Search";
 
-const TrailsContainer = ({ trails, search, setSearch }) => {
+const TrailsContainer = ({ trails, setTrails, search, setSearch }) => {
 	const displayTrails = trails.filter((trail) =>
 		trail.trailName.toLowerCase().includes(search.toLowerCase())
 	);
 
 	return (
-		<div>
-			<h1 style={{ color: "sienna" }} className="center">
-				Blaze These Trails
-			</h1>
-			<SortSearch search={search} setSearch={setSearch} />
+		<div className="trailsBg">
+			<div>
+				<h1 id="headingStyle" className="center">
+					Blaze These Trails
+				</h1>
+			</div>
+			<Search search={search} setSearch={setSearch} />
+
 			<div id="trailContainer">
 				{displayTrails.map((trail) => (
-					<TrailCard trail={trail} />
+					<TrailCard trail={trail} trails={trails} key={trail.id} />
 				))}
 			</div>
 		</div>
